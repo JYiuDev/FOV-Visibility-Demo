62ae368193a347723a855d63df30fab9e3062d0e
SenpaiJ <jason-zi-xin.yiu@students.mq.edu.au>
2024-03-03 18:02:13 +1100

    Flickering Light
    First pass

--- Real/FluctuatingLight.gd
+++ Real/FluctuatingLight.gd
@@ -1,16 +1,20 @@
 extends PointLight2D
+const myMath = preload("res://Libraries/my_math.gd")
 
-#time
+#time 
 var t: float = 0
+var defualt_energy:float = 1
 
 """
-
+Alternate the energy of the light equation mainly made out of several sin wave functions
 """
-func _process(delta):
-	
-	pass
+func _physics_process(delta):
+	t += delta
+	energy_flicker(t)
 
 #Automatically change it's energy
-func energy_flicker():
-	
-	pass
+func energy_flicker(time: float):
+	var wave1 = myMath._sin(time,1, 0.5)
+	var wave2 = myMath._sin(time, 0.5, 0.3)
+	var wave3 = myMath._sin(time, 1, 2)
+	energy = defualt_energy + ((wave1 + wave2 + wave3)*0.4)
