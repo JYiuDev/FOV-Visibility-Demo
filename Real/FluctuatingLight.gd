f4485ed6eccbed544e805e96f2924fd7ab02ff83
SenpaiJ <jason-zi-xin.yiu@students.mq.edu.au>
2024-03-06 18:13:02 +1100

    Updating Readme

--- Real/FluctuatingLight.gd
+++ Real/FluctuatingLight.gd
@@ -4,10 +4,12 @@ const myMath = preload("res://Libraries/my_math.gd")
 #time 
 var t: float = 0
 var defualt_energy:float = 1
+var default_texture_scale:float = 1
 
 """
 Alternate the energy of the light equation mainly made out of several sin wave functions
 """
+
 func _physics_process(delta):
 	t += delta
 	energy_flicker(t)
@@ -17,4 +19,5 @@ func energy_flicker(time: float):
 	var wave1 = myMath._sin(time,1, 0.5)
 	var wave2 = myMath._sin(time, 0.5, 0.3)
 	var wave3 = myMath._sin(time, 1, 2)
-	energy = defualt_energy + ((wave1 + wave2 + wave3)*0.4)
+	energy = defualt_energy + ((wave1 + wave2 + wave3)*0.6)
+	texture_scale = default_texture_scale + ((wave1 + wave2 + wave3)*0.1)
